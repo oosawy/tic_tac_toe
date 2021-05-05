3bde1796dcec91f9ceb243e45c8a6ba0b879f649
yuta <yutaosawa81@gmail.com>
2021-05-06 03:44:42 +0900

    Install tui crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::widgets::{Block, Borders};
+use tui::Terminal;
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    Ok(())
 }
