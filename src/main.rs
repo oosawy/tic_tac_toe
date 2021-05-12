f03a7965cbeaf291037e309b5adc514af36879e9
yuta <yutaosawa81@gmail.com>
2021-05-12 18:54:50 +0900

    Add a cursor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
-use tui::widgets::{Block, Borders};
+use tui::layout::Rect;
+use tui::widgets::Paragraph;
 use tui::Terminal;
 
 fn main() -> io::Result<()> {
@@ -11,20 +12,29 @@ fn main() -> io::Result<()> {
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut cursor = Rect::default();
+
     loop {
         terminal.clear()?;
 
         terminal.draw(|f| {
-            let size = f.size();
-            let block = Block::default().title("Block").borders(Borders::ALL);
-            f.render_widget(block, size);
+            let text = Paragraph::new("+");
+            f.render_widget(text, Rect::new(cursor.x, cursor.y, 3, 1));
         })?;
 
-        for event in io::stdin().keys() {
+        if let Some(event) = io::stdin().keys().into_iter().next() {
             match event? {
-                Key::Char('q') => return Ok(()),
+                Key::Char('q') => break,
+
+                Key::Left => cursor.x -= 4,
+                Key::Right => cursor.x += 4,
+                Key::Up => cursor.y -= 2,
+                Key::Down => cursor.y += 2,
+
                 _ => {}
-            }
+            };
         }
     }
+
+    Ok(())
 }
